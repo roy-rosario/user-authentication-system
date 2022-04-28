78e5fbc1a54315bf413c7831837e6c6e8044e1e4
Roy <Roy>
2022-04-28 17:35:47 -0400

    routine commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,15 @@
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function Dashboard(){
+    const redirect = useNavigate()
+
+    useEffect(()=>{
+        if(!localStorage.getItem('token')){
+            redirect("/")
+        }
+    },[])
+
     return(
         <div>
             <h1>This is the Dashboard</h1>
